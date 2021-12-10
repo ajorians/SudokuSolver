2a4677becd935faf787e7314642a99e6eec53cbb
A.J. Orians <ajorians@gmail.com>
2021-12-09 21:47:19 -0500

    Added some more puzzles and some comments.

--- SudokuSolver/SudokuSolver.h
+++ SudokuSolver/SudokuSolver.h
@@ -9,10 +9,11 @@ public:
 
    //Tries to place a single number.  Returns false if no replacement made
    bool SolveOneStep();
-   bool SolveOneMissingValue();
-   bool SolveOne3x3OnlySpotForValue();
-   bool SolveOneRowColSpotForValue();
-   bool SolveOneTryingPossibilities();
+
+   bool SolveOneMissingValue();//If only one possible value for a spot it will use it
+   bool SolveOne3x3OnlySpotForValue();//Will check it's values if any other spot in 3x3 area works
+   bool SolveOneRowColSpotForValue();//Will check if it's values if any other spot in row/col work
+   bool SolveOneTryingPossibilities();//Will use it possible values and make sure other spots have possibilites and board is still valid
    bool SolveOneTakingGuess();
 
    bool DidSolvePuzzle() const;
